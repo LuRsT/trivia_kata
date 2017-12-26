b3f801a9a93684e2ea52f4197299bb187a691f25
Gil Gonçalves <lursty@gmail.com>
2017-12-26 19:27:40 +0000

    Documenting commit:
    This is a crazy kata, lemme tell you, I had to mock print to test the
    roll method, I'm not even sure this is a good start, but I wanted to
    tackle the hardcoded category strings first. I'll discard these changes
    and try a different approach, leaving this commit for history

--- test_trivia.py
+++ test_trivia.py
@@ -1,9 +1,68 @@
+import builtins
+
 from trivia import Game
+
+
+
+class MockPrint:
+    """
+    This is to patch print and check what's happening
+
+    """
+    def __init__(self):
+        self.strings = []
+
+    def __call__(self, value):
+        self.strings.append(value)
 
 
 class TestTrivia:
 
-    def test_it(self):
+    def test_roll_for_science(self):
         game = Game()
         game.add('Chet')
-        assert len(game.players) == 2
+
+        builtins.print = MockPrint()
+        game.roll(1)
+        assert 'The category is Science' in print.strings
+
+    def test_roll_for_sports(self):
+        game = Game()
+        game.add('Chet')
+
+        builtins.print = MockPrint()
+        game.roll(2)
+        assert 'The category is Sports' in print.strings
+
+    def test_roll_for_rock(self):
+        game = Game()
+        game.add('Chet')
+
+        builtins.print = MockPrint()
+        game.roll(3)
+        assert 'The category is Rock' in print.strings
+
+    def test_roll_for_pop(self):
+        game = Game()
+        game.add('Chet')
+
+        builtins.print = MockPrint()
+        game.roll(4)
+        assert 'The category is Pop' in print.strings
+
+    def test_roll_for_science(self):
+        game = Game()
+        game.add('Chet')
+
+        builtins.print = MockPrint()
+        game.roll(5)
+        assert 'The category is Science' in print.strings
+
+    def test_roll_for_sports_2(self):
+        # wat?!
+        game = Game()
+        game.add('Chet')
+
+        builtins.print = MockPrint()
+        game.roll(6)
+        assert 'The category is Sports' in print.strings
